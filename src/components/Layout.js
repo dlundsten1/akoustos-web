b7cb8fb5f2769848e6e1e4bd656eb2d9d829d55e
David Lundsten <david.lundsten@ffcg.se>
2021-08-06 15:11:19 +0200

    remove window reference

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,16 +2,17 @@ import React from "react";
 import * as styles from "./styles/layout.module.css";
 import "./global.css";
 import { Header, Footer } from ".";
-var classNames = require('classnames/bind');
-var cx = classNames.bind(styles); 
+var classNames = require("classnames/bind");
+var cx = classNames.bind(styles);
 
 const layout = ({ children }) => {
   return (
     <>
-      <Header/>
-      <div className={cx('root',{
+      <Header />
+      {/* <div className={cx('root',{
         'black': window && window.location.pathname === '/'
-      })}>
+      })}> */}
+      <div className={cx("root")}>
         <div className={styles.container}>{children}</div>
         {/* <Footer /> */}
       </div>
