19b887d5729a8c7bfccccee7ffad8b472b696989
David Lundsten <david.lundsten@ffcg.se>
2021-08-16 22:07:04 +0200

    Fetchings news from contentful

--- src/pages/news.js
+++ src/pages/news.js
@@ -1,97 +1,71 @@
 import React from "react";
 import Layout from "../components/Layout";
+import { graphql, Link, useStaticQuery } from "gatsby";
 import * as styles from "./news.module.css";
 // import { StaticImage } from "gatsby-image"
 
-const home = () => {
+const Article = ({ node }) => {
+  return (
+    <div className={styles.article}>
+      <div></div>
+      <img
+        className={styles.image}
+        alt="image of the Akoustos screw"
+        src={node.image.file.url}
+      />
+      <div className={styles.details}>
+        <h3>{node.title}</h3>
+        <p>{node.content.raw}</p>
+
+        <div className={styles.divider}></div>
+        <div className={styles.footer}>
+          {node.createdAt} • By{" "}
+          <span className={styles.name}> {node.author} </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+const News = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allContentfulArticle {
+        edges {
+          node {
+            id
+            createdAt
+            contentful_id
+            children {
+              id
+            }
+            author
+            image {
+              file {
+                url
+              }
+            }
+            title
+            content {
+              raw
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  const articles = data.allContentfulArticle.edges;
+  console.log(articles);
   return (
     <Layout onRoot={false}>
       <h1>News</h1>
       <div className={styles.news}>
-        <div className={styles.article}>
-          <div></div>
-          <img
-            className={styles.image}
-            alt="image of the Akoustos screw"
-            src={`screwfinger.webp`}
-          />
-          <div className={styles.details}>
-            <h3>
-              The London based startup attracted Fortune 500 companies with
-              their new website on Webflow
-            </h3>
-            <p>
-              Fanbytes need a new website that reflects their new branding and
-              expanding services about influencer marketing. They also want to
-              update their product pages, publish new case studies and blog
-              posts regularly. I redesigned their website, customized Webflow
-              CMS by considering their needs and finally they got an easy to use
-              website on Webflow.
-            </p>
-
-            <div className={styles.divider}></div>
-            <div className={styles.footer}>
-              May 2020 • By <span className={styles.name}> Håkan </span>
-            </div>
-          </div>
-        </div>
-        <div className={styles.article}>
-          <div></div>
-          <img
-            className={styles.image}
-            alt="image of the Akoustos screw"
-            src={`screwfinger.webp`}
-          />
-          <div className={styles.details}>
-            <h3>
-              The London based startup attracted Fortune 500 companies with
-              their new website on Webflow
-            </h3>
-            <p>
-              Fanbytes need a new website that reflects their new branding and
-              expanding services about influencer marketing. They also want to
-              update their product pages, publish new case studies and blog
-              posts regularly. I redesigned their website, customized Webflow
-              CMS by considering their needs and finally they got an easy to use
-              website on Webflow.
-            </p>
-
-            <div className={styles.divider}></div>
-            <div className={styles.footer}>
-              May 2020 • By <span className={styles.name}> Håkan </span>
-            </div>
-          </div>
-        </div>
-        <div className={styles.article}>
-          <div></div>
-          <img
-            className={styles.image}
-            alt="image of the Akoustos screw"
-            src={`screwfinger.webp`}
-          />
-          <div className={styles.details}>
-            <h3>
-              The London based startup attracted Fortune 500 companies with
-              their new website on Webflow
-            </h3>
-            <p>
-              Fanbytes need a new website that reflects their new branding and
-              expanding services about influencer marketing. They also want to
-              update their product pages, publish new case studies and blog
-              posts regularly. I redesigned their website, customized Webflow
-              CMS by considering their needs and finally they got an easy to use
-              website on Webflow.
-            </p>
-
-            <div className={styles.divider}></div>
-            <div className={styles.footer}>
-              May 2020 • By <span className={styles.name}> Håkan </span>
-            </div>
-          </div>
-        </div>
+        {articles.map((article) => {
+          return <Article node={article.node} />;
+        })}
       </div>
     </Layout>
   );
 };
 
-export default home;
+export default News;
